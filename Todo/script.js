746454062ef0c819ea804fda792fbceff592accf
Ankit Kumar <kumaarankit05@gmail.com>
2026-01-27 01:10:44 +0530

    new changes

--- Todo/script.js
+++ Todo/script.js
@@ -4,44 +4,120 @@ let titleInput = document.querySelector(".js-input");
 let textArea = document.querySelector(".js-textarea");
 let submitButton = document.querySelector(".js-button");
 
+function clearFields() {
+  titleInput.value = "";
+  textArea.value = "";
+}
 
+submitButton?.addEventListener("click", (event) => {
+  event.preventDefault();
 
-submitButton?.addEventListener("click", () => {
   let dataObject = {
     title: titleInput.value,
     text: textArea.value,
   };
 
-  localStorage.setItem("taskData", JSON.stringify(dataObject));
+  // 1. Get existing tasks OR empty array
+  let existingTasks = JSON.parse(localStorage.getItem("taskData")) || [];
+
+  // 2. Add new task to array
+  existingTasks.push(dataObject);
+
+  localStorage.setItem("taskData", JSON.stringify(existingTasks));
+  clearFields();
   window.location.href = "Dashboard.html";
 });
 
-
 // Dashboard Logic
 
-// let noteHeading = document.querySelector(".js-heading");
 let noteDiv = document.querySelector(".js-notes-container");
+
 if (noteDiv) {
-    const storedData = localStorage.getItem("taskData");
-    console.log("storedData:", storedData);
-    if(storedData){
-        const parsedData = JSON.parse(storedData)
-        const noteCard = createNotesCard(parsedData.title)
-        noteDiv.append(noteCard)
-    }
+  let storedData = JSON.parse(localStorage.getItem("taskData")) || [];
+
+  storedData.forEach((task) =>{
+    let noteCard = createNotesCard(task.title, task.text);
+    noteDiv.append(noteCard)
+  });
 }
 
-function createNotesCard(title) {
+//   console.log("storedData:", storedData);
+//   if (storedData) {
+//     const parsedData = JSON.parse(storedData); // it contains object dataOject
+//     const noteCard = createNotesCard(parsedData.title, parsedData.text);
+//     noteDiv.append(noteCard);
+//   }
+// }
+
+function createNotesCard(title, para) {
   const parentDiv = document.createElement("div");
 
   parentDiv.className =
-    "bg-yellow-300 border border-[#A1A3AB] rounded-xl w-96 h-[166px] p-4";
+    "bg-[#F5F8FF] border border-[#A1A3AB] rounded-xl w-96 h-[166px] p-4";
 
   const heading = document.createElement("h2");
   heading.className = "text-base font-semibold";
-  heading.textContent = title;
+  const paragraph = document.createElement("p");
+  paragraph.className = "text-sm text-[#747474]";
 
-  parentDiv.append(heading);
+  heading.textContent = title;
+  paragraph.textContent = para;
+
+  parentDiv.append(heading, paragraph);
 
   return parentDiv;
 }
+
+// // Tadd New Task Logic
+
+// let titleInput = document.querySelector(".js-input");
+// let textArea = document.querySelector(".js-textarea");
+// let submitButton = document.querySelector(".js-button");
+
+// function clearFields() {
+//   titleInput.value = "";
+//   textArea.value = "";
+// }
+
+// submitButton?.addEventListener("click", () => {
+//   let dataObject = {
+//     title: titleInput.value,
+//     text: textArea.value,
+//   };
+//   clearFields();
+//   localStorage.setItem("taskData", JSON.stringify(dataObject));
+//   window.location.href = "Dashboard.html";
+// });
+
+// // Dashboard Logic
+
+// // let noteHeading = document.querySelector(".js-heading");
+// let noteDiv = document.querySelector(".js-notes-container");
+// if (noteDiv) {
+//   const storedData = localStorage.getItem("taskData");
+//   console.log("storedData:", storedData);
+//   if (storedData) {
+//     const parsedData = JSON.parse(storedData); // it contains object dataOject
+//     const noteCard = createNotesCard(parsedData.title, parsedData.text);
+//     noteDiv.append(noteCard);
+//   }
+// }
+
+// function createNotesCard(title, para) {
+//   const parentDiv = document.createElement("div");
+
+//   parentDiv.className =
+//     "bg-[#F5F8FF] border border-[#A1A3AB] rounded-xl w-96 h-[166px] p-4";
+
+//   const heading = document.createElement("h2");
+//   heading.className = "text-base font-semibold";
+//   const paragraph = document.createElement("p");
+//   paragraph.className = "text-sm text-[#747474]";
+
+//   heading.textContent = title;
+//   paragraph.textContent = para;
+
+//   parentDiv.append(heading, paragraph);
+
+//   return parentDiv;
+// }
