f8ad95e4bbe2cc0feffc33e2502256b5e1ec52d4
Ankit Kumar <kumaarankit05@gmail.com>
2026-01-29 02:42:34 +0530

    created pop up new-task

--- Todo/script.js
+++ Todo/script.js
@@ -3,6 +3,12 @@
 let titleInput = document.querySelector(".js-input");
 let textArea = document.querySelector(".js-textarea");
 let submitButton = document.querySelector(".js-button");
+let addTask = document.querySelector(".js-addTask");
+let taskModal = document.querySelector(".js-addTaskModal");  // Add task 
+
+addTask.addEventListener("click", () =>{
+  taskModal.classList.remove("hidden")
+})
 
 function clearFields() {
   titleInput.value = "";
@@ -25,8 +31,9 @@ submitButton?.addEventListener("click", (event) => {
 
   localStorage.setItem("taskData", JSON.stringify(existingTasks));
   clearFields();
-  window.location.href = "Dashboard.html";
+  // window.location.href = "Dashboard.html"; // commneted it out because now, the psge is working as a popup on the dashbaord
 });
+
 
 // Dashboard Logic
 
@@ -35,30 +42,22 @@ let noteDiv = document.querySelector(".js-notes-container");
 if (noteDiv) {
   let storedData = JSON.parse(localStorage.getItem("taskData")) || [];
 
-  storedData.forEach((task) =>{
+  storedData.forEach((task) => {
     let noteCard = createNotesCard(task.title, task.text);
-    noteDiv.append(noteCard)
+    noteDiv.append(noteCard);
   });
 }
-
-//   console.log("storedData:", storedData);
-//   if (storedData) {
-//     const parsedData = JSON.parse(storedData); // it contains object dataOject
-//     const noteCard = createNotesCard(parsedData.title, parsedData.text);
-//     noteDiv.append(noteCard);
-//   }
-// }
 
 function createNotesCard(title, para) {
   const parentDiv = document.createElement("div");
 
   parentDiv.className =
-    "bg-[#F5F8FF] border border-[#A1A3AB] rounded-xl w-96 h-[166px] p-4";
+    "bg-[#F5F8FF] mt-2 mb-2 border border-[#A1A3AB] rounded-xl w-sm h-[166px] p-4";
 
   const heading = document.createElement("h2");
-  heading.className = "text-base font-semibold";
+  heading.className = "text-base font-semibold text-lg leading-tight";
   const paragraph = document.createElement("p");
-  paragraph.className = "text-sm text-[#747474]";
+  paragraph.className = "text-sm text-[#747474] mt-2 w-64";
 
   heading.textContent = title;
   paragraph.textContent = para;
@@ -67,57 +66,3 @@ function createNotesCard(title, para) {
 
   return parentDiv;
 }
-
-// // Tadd New Task Logic
-
-// let titleInput = document.querySelector(".js-input");
-// let textArea = document.querySelector(".js-textarea");
-// let submitButton = document.querySelector(".js-button");
-
-// function clearFields() {
-//   titleInput.value = "";
-//   textArea.value = "";
-// }
-
-// submitButton?.addEventListener("click", () => {
-//   let dataObject = {
-//     title: titleInput.value,
-//     text: textArea.value,
-//   };
-//   clearFields();
-//   localStorage.setItem("taskData", JSON.stringify(dataObject));
-//   window.location.href = "Dashboard.html";
-// });
-
-// // Dashboard Logic
-
-// // let noteHeading = document.querySelector(".js-heading");
-// let noteDiv = document.querySelector(".js-notes-container");
-// if (noteDiv) {
-//   const storedData = localStorage.getItem("taskData");
-//   console.log("storedData:", storedData);
-//   if (storedData) {
-//     const parsedData = JSON.parse(storedData); // it contains object dataOject
-//     const noteCard = createNotesCard(parsedData.title, parsedData.text);
-//     noteDiv.append(noteCard);
-//   }
-// }
-
-// function createNotesCard(title, para) {
-//   const parentDiv = document.createElement("div");
-
-//   parentDiv.className =
-//     "bg-[#F5F8FF] border border-[#A1A3AB] rounded-xl w-96 h-[166px] p-4";
-
-//   const heading = document.createElement("h2");
-//   heading.className = "text-base font-semibold";
-//   const paragraph = document.createElement("p");
-//   paragraph.className = "text-sm text-[#747474]";
-
-//   heading.textContent = title;
-//   paragraph.textContent = para;
-
-//   parentDiv.append(heading, paragraph);
-
-//   return parentDiv;
-// }
